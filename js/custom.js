ad0909371b51f266b81d0ae0d8e3fe613460a246
enakee <enakee@naver.com>
2017-02-15 16:20:21 +0900

    front-end_upload_cc
    디자인 및 퍼블리싱 1차 완료 업로드

---
+++ js/custom.js
@@ -0,0 +1,164 @@
+//모바일 GNB스크립트//
+jQuery(document).ready(function() {
+  $('#Gnb-downMenu1 span').click(function() {
+    $('#Gnb-downMenu1').children('li').toggle()
+  })
+  $('#Gnb-downMenu2 span').click(function() {
+    $('#Gnb-downMenu2').children('li').toggle()
+  })
+  $('#Gnb-downMenu3 span').click(function() {
+    $('#Gnb-downMenu3').children('li').toggle()
+  })
+    });
+jQuery(document).ready(function() {
+    $("#gnb-nav-btn").on("click", function() {
+      $("#Gnb-frame").addClass("gnb-active");
+      $(this).hide();
+    });
+    $("#gnb-nav-close").on("click", function() {
+      $("#Gnb-frame").removeClass("gnb-active");
+      $("#gnb-nav-btn").show();
+    });
+  });
+//GNB스크립트//
+$(function(){
+  $(".zeta-menu li").hover(function() {
+    $('ul:first', this).show();
+  }, function() {
+    $('ul:first', this).hide();
+  });
+  $(".zeta-menu ul li:has(ul)").find("a:first").append("<p style='float:right;margin:-3px'>&#9656;</p>");
+})
+//텍스트 입력기 html,text 선택스크립트//
+$(function() {
+  $(".flex-box-inner span").click(function() {
+    if ($(this).hasClass('active')) {} else {
+      $(".flex-box-inner span").removeClass("active");
+      $(this).addClass("active");
+    }
+  });
+});
+//리플 기능 스크립트//
+$(function() {
+  $(".show-reply").click(function() {
+    if ($(this).hasClass('active')) {
+      $(".reply").addClass('hidden');
+      $(".close-reply").addClass('hidden');
+      $(".open-reply").removeClass('hidden');
+      $(this).removeClass("active");
+    } else {
+      $(".open-reply").addClass('hidden');
+      $(".close-reply").removeClass('hidden');
+      $(".reply").removeClass("hidden");
+      $(this).addClass("active");
+    }
+  });
+});
+//검색 버튼 스크립트//
+jQuery(document).ready(function() {
+  $(".search_button").on("click", function() {
+    if ($(this).hasClass("active")) {} else {
+      $(this).addClass("active");
+      $(".search_wrap").addClass("active");
+      $(".search_bottun i").addClass("active");
+      $("input").addClass("active");
+    }
+  });
+});
+
+//차시 버튼 스크립트//
+$(function() {
+  $(".chasi ul li").click(function() {
+    if ($(this).hasClass('active')) {} else {
+      $(".chasi ul li").removeClass("chasi-active");
+      $(this).addClass('chasi-active');
+    }
+  });
+});
+
+//차시 페이지 스크립트 //
+$(document).ready(function() {
+  $("#page_01").show();
+  $("#page_02").hide();
+  $("#page_03").hide();
+  $("#page_04").hide();
+  $("#page_05").hide();
+  $("#page_06").hide();
+  $("#page_07").hide();
+  $("#page_08").hide();
+
+  $(".chasi-btn-01").click(function() {
+    $("#page_01").show();
+    $("#page_02").hide();
+    $("#page_03").hide();
+    $("#page_04").hide();
+    $("#page_05").hide();
+    $("#page_06").hide();
+    $("#page_07").hide();
+    $("#page_08").hide();
+  });
+
+  $(".chasi-btn-02").click(function() {
+    $("#page_01").hide();
+    $("#page_02").show();
+    $("#page_03").hide();
+    $("#page_04").hide();
+    $("#page_05").hide();
+    $("#page_06").hide();
+    $("#page_07").hide();
+    $("#page_08").hide();
+  });
+});
+
+//진도 현황//
+$(document).ready(function() {
+  $(".current-playing").hide();
+
+  $(".progress-org li").click(function() {
+    $(".ready-playing").show();
+    $(".ready-playing", this).hide();
+    $(".current-playing").hide();
+    $(".current-playing", this).show();
+  });
+});
+//진도 현황-반선택//
+$(function() {
+  $(".class-name").click(function() {
+    if ($(this).hasClass('active')) {} else {
+      $(".class-name").removeClass("chasi-active");
+      $(this).addClass('chasi-active');
+    }
+  });
+});
+// //
+$(document).ready(function() {
+
+  $("div.select > a").click(function() {
+    $(this).next("ul").toggle();
+    return false;
+  });
+
+  $("div.select > ul > li").click(function() {
+    $(this).parent().hide() //.parent("div.select").children("a").text($(this).text());
+    //    $(this).prependTo($(this).parent());
+  });
+});
+//메인슬라이더
+$(document).ready(function() {
+  $('.slider1').bxSlider({slideWidth: 180, minSlides: 1, maxSlides: 4, slideMargin: 20});
+});
+
+//갤러리 드랍다운//
+$(document).ready(function() {
+  $('.select-label').click(function() {
+    $('.dropdown').toggleClass('active');
+    $('.dropdown-list li').toggleClass('show');
+  });
+  $('.dropdown-list li').click(function() {
+    $('.select-label').text($(this).text());
+    $('.dropdown').removeClass('active');
+      $('.dropdown-list li').toggleClass('show');
+  });
+
+
+});
