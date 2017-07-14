cb78ff8c3dc87a19015d5167f714a5de5c4d82c9
makerslab2017 <beomjoo90@gmail.com>
2017-07-14 09:49:43 +0900

    슬라이드 이벤트 재설정기능을 해당 html 로딩시 처리
    개별 퍼즐 html별로 처리할 필요가 없어짐.

--- js/custom.js
+++ js/custom.js
@@ -240,6 +240,7 @@ $(document).ready(function() {
 
 function loadStage(obj) {
   if (obj == null) return;
+  if (Module.missionCompleteProcessing == true) return;
   $(".ready-playing", obj).hide();
   $(".current-playing", obj).show();
   Module.obj = $(obj);
@@ -249,6 +250,11 @@ function loadStage(obj) {
 
   $.get('./stage/' + Module.stage + '.html', function(data) {
     $('.slidePage').empty().append(data);
+    $('.chasi-info-prev').on('click', function() { $('.carousel').carousel('prev');});
+    $('.chasi-info-next').on('click', function() { $('.carousel').carousel('next');});
+    $('.carousel-indicators > li').on('click', function() { 
+        $('.carousel').carousel( $(this).parent('.carousel-indicators').find('li').index( $(this) ) );
+    });
   });
  
   if (Module.stage != 'latest' && Module.stage != 'puzzleLatest') {
@@ -282,12 +288,15 @@ function loadModule() {
       obj : null,
       nextObj : null,
       robotLoaded : false,
+      missionCompleteProcessing: false,
       OnMissionComplete: function() {  
+        missionCompleteProcessing = true;
         if (Module.obj == null) return;
         $(".ready-playing", Module.obj[0]).show();
         $(".current-playing", Module.obj[0]).hide();
         var src = Module.obj.find(".ready-playing").attr('src');
         $(".ready-playing", Module.obj[0]).attr('src', src.replace('.png', '-clear.png'));
+        missionCompleteProcessing = false;
         if (Module.nextObj == null) return;
         setTimeout( function() {  loadStage(Module.nextObj); }, 4000);
       },
